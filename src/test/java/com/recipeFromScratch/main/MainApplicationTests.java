f05a5b97fed841e13d1b7accf616ee371e66aaee
Mayuri <Mayuri@localhost>
2021-01-16 14:32:42 +0530

    Initial Commit for Recipe From Scratch

---
+++ src/test/java/com/recipeFromScratch/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipeFromScratch.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
